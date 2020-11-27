cb0370bc277d8230e3a93ac422862159bd1a5585
Wu Yuzhe <a0903153387@yahoo.com.tw>
2020-11-27 22:48:33 +0800

    練習8.2:在posts頁面的子標題限制字數150個字

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,7 +30,7 @@
                          {{ $post->title }}
                     </h2>
                     <h3 class="post-subtitle">
-                        Subtitle
+                        {{Str::limit($post->content,150)}}
                     </h3>
                 </a>
                 <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
